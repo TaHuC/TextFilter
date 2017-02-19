966e1a7f07ca5dccd19a7ac1f36f62b9ea5b00a9
TaHuC <TaHuC@hotmail.com>
2017-02-19 07:34:26 +0200

    Complate

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace TextFilter
 {
@@ -6,7 +7,26 @@ namespace TextFilter
 	{
 		public static void Main()
 		{
-			
+			string[] getCensure = Console.ReadLine()
+										 .Split(',', (char)StringSplitOptions.RemoveEmptyEntries)
+										 .ToArray();
+
+			var getAllText = Console.ReadLine();
+
+			for (var i = 0; i < getCensure.Length; i++)
+			{
+				var starts = getCensure[i].Trim().Length;
+				var setStars = string.Empty;
+
+				for (var j = 0; j < starts; j++)
+				{
+					setStars += "*";
+				}
+
+				getAllText = getAllText.Replace(getCensure[i].Trim(), setStars);
+			}
+
+			Console.WriteLine(getAllText);
 		}
 	}
 }
